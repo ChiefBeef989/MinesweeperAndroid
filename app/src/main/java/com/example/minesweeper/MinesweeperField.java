e9baca7cba22391064849ff884ee2b54d3ccba4d
Marlon Seifert <marlon.seifert@edu.bib.de>
2020-12-19 14:46:08 +0100

    Milestone #2

--- app/src/main/java/com/example/minesweeper/MinesweeperField.java
+++ app/src/main/java/com/example/minesweeper/MinesweeperField.java
@@ -5,4 +5,45 @@ public class MinesweeperField {
     private boolean isFlagged;
     private boolean isMined;
     private int adjacentMines;
+
+    private float startX, stopX, startY, stopY;
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public boolean isMined() {
+        return isMined;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public float getStartX() {
+        return startX;
+    }
+
+    public float getStopX() {
+        return stopX;
+    }
+
+    public float getStartY() {
+        return startY;
+    }
+
+    public float getStopY() {
+        return stopY;
+    }
+
+    public MinesweeperField(float startX, float stopX, float startY, float stopY){
+        this.startX = startX;
+        this.stopX = stopX;
+        this.startY = startY;
+        this.stopY = stopY;
+    }
 }
--- app/src/main/java/com/example/minesweeper/MinesweeperView.java
+++ app/src/main/java/com/example/minesweeper/MinesweeperView.java
@@ -5,9 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.time.Duration;
 
 public class MinesweeperView extends View {
 
@@ -15,6 +19,8 @@ public class MinesweeperView extends View {
     float cWidth;
     float fieldHeight;
     float fieldWidth;
+
+    MinesweeperField[][] fields = new MinesweeperField[10][10];
 
     Paint p;
 
@@ -33,5 +39,27 @@ public class MinesweeperView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        setBackgroundColor(Color.BLACK);
+
+        p = new Paint();
+        p.setColor(Color.WHITE);
+        p.setStrokeWidth(4f);
+
+        Paint b = new Paint();
+        b.setColor(Color.GRAY);
+
+        cHeight = canvas.getHeight();
+        cWidth = canvas.getWidth();
+
+        fieldHeight = cHeight / 10;
+        fieldWidth = cWidth / 10;
+
+        for(int i = 1; i < 10; i++){
+            canvas.drawLine(i*fieldWidth,0f,i*fieldWidth,cHeight,p);
+            canvas.drawLine(0,i*fieldHeight,cWidth,i*fieldHeight,p);
+        }
+
+
     }
 }
